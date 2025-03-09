99ee8d0b14af1efa726cf961865d380e86319b75
Forem011 <forem011@csusm.edu>
2025-03-08 21:12:58 -0800

    A5.cpp

--- A5.cpp
+++ A5.cpp
@@ -15,7 +15,7 @@ ifstream file("input.txt");
 string word;
 while (file >> word)
   {
-    string cleanword;
+    string cleanword; //will convert all words to lowercase and removes symbols
     for (char c : word) //processes each letter in the loop of the current word
       {
 	if (isalpha(c))// checks if character is a letter, gets rid of punctuation marks
@@ -26,19 +26,19 @@ while (file >> word)
     if (!cleanword.empty())
       {
 	bool found = false;// checks if word is already in vector
-	for (auto& pair : wordCount)
+	for (pair<string, int>& pair : wordCount) 
 	  {
-	    if (pair.first == cleanword)
+	    if (pair.first == cleanword) // if the word is found in the vector then 1 is added to the frequency count
 	      {
 		pair.second++;
 		found = true;
 	      }
 	  }
-	if (!found)
+	if (!found) // if it is not found in the vector then the word is displayed and 1 is added to its other pair
 	  {
 	    wordCount.push_back({cleanword, 1});
       }
-	cout << "Processing word: " << cleanword << endl; 
+	cout << "Processing word: " << cleanword << endl; // added to troubleshoot if the function was only reading the first word or only displaying the first word
       }
   }
 	file.close();
@@ -46,14 +46,14 @@ while (file >> word)
 void writeWordFrequencies(const vector<pair<string, int>>& wordCount, const string& outputFile)
 {
   ofstream output_file ("output.txt");
-  if (!output_file.is_open())
+  if (!output_file.is_open())// checks if output file was created and opened
     {
       cout << "Error opening output file" << endl;
       return;
     }
-  for (const auto& pair : wordCount)
+  for (const pair<string, int>& pair : wordCount)
     {
-      output_file << pair.first << ": " << pair.second << endl;
+      output_file << pair.first << ": " << pair.second << endl; // makes the word: frequency format then goes to next line
     }
   output_file.close();
     }
